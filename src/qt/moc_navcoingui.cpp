b038ee0cfc808d20eaaf13310e37c80e1eb52fa4
momowiz <momo-dev@relayofficial.org>
2018-02-21 00:33:57 -0600

    Class components, Final Relay Core GUI v1

---
+++ src/qt/moc_navcoingui.cpp
@@ -0,0 +1,463 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'navcoingui.h'
+**
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include "qt/navcoingui.h"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'navcoingui.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 67
+#error "This file was generated using the moc from 5.9.2. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+struct qt_meta_stringdata_NavCoinGUI_t {
+    QByteArrayData data[62];
+    char stringdata0[815];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    qptrdiff(offsetof(qt_meta_stringdata_NavCoinGUI_t, stringdata0) + ofs \
+        - idx * sizeof(QByteArrayData)) \
+    )
+static const qt_meta_stringdata_NavCoinGUI_t qt_meta_stringdata_NavCoinGUI = {
+    {
+QT_MOC_LITERAL(0, 0, 10), // "NavCoinGUI"
+QT_MOC_LITERAL(1, 11, 11), // "receivedURI"
+QT_MOC_LITERAL(2, 23, 0), // ""
+QT_MOC_LITERAL(3, 24, 3), // "uri"
+QT_MOC_LITERAL(4, 28, 17), // "setNumConnections"
+QT_MOC_LITERAL(5, 46, 5), // "count"
+QT_MOC_LITERAL(6, 52, 12), // "setNumBlocks"
+QT_MOC_LITERAL(7, 65, 9), // "blockDate"
+QT_MOC_LITERAL(8, 75, 21), // "nVerificationProgress"
+QT_MOC_LITERAL(9, 97, 7), // "headers"
+QT_MOC_LITERAL(10, 105, 7), // "message"
+QT_MOC_LITERAL(11, 113, 5), // "title"
+QT_MOC_LITERAL(12, 119, 5), // "style"
+QT_MOC_LITERAL(13, 125, 5), // "bool*"
+QT_MOC_LITERAL(14, 131, 3), // "ret"
+QT_MOC_LITERAL(15, 135, 13), // "replyFinished"
+QT_MOC_LITERAL(16, 149, 14), // "QNetworkReply*"
+QT_MOC_LITERAL(17, 164, 5), // "reply"
+QT_MOC_LITERAL(18, 170, 19), // "replyVotingFinished"
+QT_MOC_LITERAL(19, 190, 18), // "startVotingCounter"
+QT_MOC_LITERAL(20, 209, 19), // "setEncryptionStatus"
+QT_MOC_LITERAL(21, 229, 6), // "status"
+QT_MOC_LITERAL(22, 236, 20), // "handlePaymentRequest"
+QT_MOC_LITERAL(23, 257, 18), // "SendCoinsRecipient"
+QT_MOC_LITERAL(24, 276, 9), // "recipient"
+QT_MOC_LITERAL(25, 286, 19), // "incomingTransaction"
+QT_MOC_LITERAL(26, 306, 4), // "date"
+QT_MOC_LITERAL(27, 311, 4), // "unit"
+QT_MOC_LITERAL(28, 316, 7), // "CAmount"
+QT_MOC_LITERAL(29, 324, 6), // "amount"
+QT_MOC_LITERAL(30, 331, 4), // "type"
+QT_MOC_LITERAL(31, 336, 7), // "address"
+QT_MOC_LITERAL(32, 344, 5), // "label"
+QT_MOC_LITERAL(33, 350, 19), // "onCurrencySelection"
+QT_MOC_LITERAL(34, 370, 8), // "QAction*"
+QT_MOC_LITERAL(35, 379, 6), // "action"
+QT_MOC_LITERAL(36, 386, 16), // "gotoOverviewPage"
+QT_MOC_LITERAL(37, 403, 15), // "gotoHistoryPage"
+QT_MOC_LITERAL(38, 419, 20), // "gotoReceiveCoinsPage"
+QT_MOC_LITERAL(39, 440, 17), // "gotoSendCoinsPage"
+QT_MOC_LITERAL(40, 458, 4), // "addr"
+QT_MOC_LITERAL(41, 463, 22), // "gotoRequestPaymentPage"
+QT_MOC_LITERAL(42, 486, 18), // "gotoSignMessageTab"
+QT_MOC_LITERAL(43, 505, 20), // "gotoVerifyMessageTab"
+QT_MOC_LITERAL(44, 526, 11), // "openClicked"
+QT_MOC_LITERAL(45, 538, 19), // "updateStakingStatus"
+QT_MOC_LITERAL(46, 558, 11), // "updatePrice"
+QT_MOC_LITERAL(47, 570, 13), // "getVotingInfo"
+QT_MOC_LITERAL(48, 584, 12), // "repairWallet"
+QT_MOC_LITERAL(49, 597, 14), // "optionsClicked"
+QT_MOC_LITERAL(50, 612, 12), // "aboutClicked"
+QT_MOC_LITERAL(51, 625, 15), // "showDebugWindow"
+QT_MOC_LITERAL(52, 641, 30), // "showDebugWindowActivateConsole"
+QT_MOC_LITERAL(53, 672, 22), // "showHelpMessageClicked"
+QT_MOC_LITERAL(54, 695, 13), // "toggleStaking"
+QT_MOC_LITERAL(55, 709, 21), // "showNormalIfMinimized"
+QT_MOC_LITERAL(56, 731, 13), // "fToggleHidden"
+QT_MOC_LITERAL(57, 745, 12), // "toggleHidden"
+QT_MOC_LITERAL(58, 758, 14), // "detectShutdown"
+QT_MOC_LITERAL(59, 773, 12), // "showProgress"
+QT_MOC_LITERAL(60, 786, 9), // "nProgress"
+QT_MOC_LITERAL(61, 796, 18) // "setTrayIconVisible"
+
+    },
+    "NavCoinGUI\0receivedURI\0\0uri\0"
+    "setNumConnections\0count\0setNumBlocks\0"
+    "blockDate\0nVerificationProgress\0headers\0"
+    "message\0title\0style\0bool*\0ret\0"
+    "replyFinished\0QNetworkReply*\0reply\0"
+    "replyVotingFinished\0startVotingCounter\0"
+    "setEncryptionStatus\0status\0"
+    "handlePaymentRequest\0SendCoinsRecipient\0"
+    "recipient\0incomingTransaction\0date\0"
+    "unit\0CAmount\0amount\0type\0address\0label\0"
+    "onCurrencySelection\0QAction*\0action\0"
+    "gotoOverviewPage\0gotoHistoryPage\0"
+    "gotoReceiveCoinsPage\0gotoSendCoinsPage\0"
+    "addr\0gotoRequestPaymentPage\0"
+    "gotoSignMessageTab\0gotoVerifyMessageTab\0"
+    "openClicked\0updateStakingStatus\0"
+    "updatePrice\0getVotingInfo\0repairWallet\0"
+    "optionsClicked\0aboutClicked\0showDebugWindow\0"
+    "showDebugWindowActivateConsole\0"
+    "showHelpMessageClicked\0toggleStaking\0"
+    "showNormalIfMinimized\0fToggleHidden\0"
+    "toggleHidden\0detectShutdown\0showProgress\0"
+    "nProgress\0setTrayIconVisible"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_NavCoinGUI[] = {
+
+ // content:
+       7,       // revision
+       0,       // classname
+       0,    0, // classinfo
+      39,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       1,       // signalCount
+
+ // signals: name, argc, parameters, tag, flags
+       1,    1,  209,    2, 0x06 /* Public */,
+
+ // slots: name, argc, parameters, tag, flags
+       4,    1,  212,    2, 0x0a /* Public */,
+       6,    4,  215,    2, 0x0a /* Public */,
+      10,    4,  224,    2, 0x0a /* Public */,
+      10,    3,  233,    2, 0x2a /* Public | MethodCloned */,
+      15,    1,  240,    2, 0x0a /* Public */,
+      18,    1,  243,    2, 0x0a /* Public */,
+      19,    0,  246,    2, 0x0a /* Public */,
+      20,    1,  247,    2, 0x0a /* Public */,
+      22,    1,  250,    2, 0x0a /* Public */,
+      25,    6,  253,    2, 0x0a /* Public */,
+      33,    1,  266,    2, 0x0a /* Public */,
+      36,    0,  269,    2, 0x08 /* Private */,
+      37,    0,  270,    2, 0x08 /* Private */,
+      38,    0,  271,    2, 0x08 /* Private */,
+      39,    1,  272,    2, 0x08 /* Private */,
+      39,    0,  275,    2, 0x28 /* Private | MethodCloned */,
+      41,    0,  276,    2, 0x08 /* Private */,
+      42,    1,  277,    2, 0x08 /* Private */,
+      42,    0,  280,    2, 0x28 /* Private | MethodCloned */,
+      43,    1,  281,    2, 0x08 /* Private */,
+      43,    0,  284,    2, 0x28 /* Private | MethodCloned */,
+      44,    0,  285,    2, 0x08 /* Private */,
+      45,    0,  286,    2, 0x08 /* Private */,
+      46,    0,  287,    2, 0x08 /* Private */,
+      47,    0,  288,    2, 0x08 /* Private */,
+      48,    0,  289,    2, 0x08 /* Private */,
+      49,    0,  290,    2, 0x08 /* Private */,
+      50,    0,  291,    2, 0x08 /* Private */,
+      51,    0,  292,    2, 0x08 /* Private */,
+      52,    0,  293,    2, 0x08 /* Private */,
+      53,    0,  294,    2, 0x08 /* Private */,
+      54,    0,  295,    2, 0x08 /* Private */,
+      55,    1,  296,    2, 0x08 /* Private */,
+      55,    0,  299,    2, 0x28 /* Private | MethodCloned */,
+      57,    0,  300,    2, 0x08 /* Private */,
+      58,    0,  301,    2, 0x08 /* Private */,
+      59,    2,  302,    2, 0x08 /* Private */,
+      61,    1,  307,    2, 0x08 /* Private */,
+
+ // signals: parameters
+    QMetaType::Void, QMetaType::QString,    3,
+
+ // slots: parameters
+    QMetaType::Void, QMetaType::Int,    5,
+    QMetaType::Void, QMetaType::Int, QMetaType::QDateTime, QMetaType::Double, QMetaType::Bool,    5,    7,    8,    9,
+    QMetaType::Void, QMetaType::QString, QMetaType::QString, QMetaType::UInt, 0x80000000 | 13,   11,   10,   12,   14,
+    QMetaType::Void, QMetaType::QString, QMetaType::QString, QMetaType::UInt,   11,   10,   12,
+    QMetaType::Void, 0x80000000 | 16,   17,
+    QMetaType::Void, 0x80000000 | 16,   17,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::Int,   21,
+    QMetaType::Bool, 0x80000000 | 23,   24,
+    QMetaType::Void, QMetaType::QString, QMetaType::Int, 0x80000000 | 28, QMetaType::QString, QMetaType::QString, QMetaType::QString,   26,   27,   29,   30,   31,   32,
+    QMetaType::Void, 0x80000000 | 34,   35,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::QString,   40,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::QString,   40,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::QString,   40,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::Bool,   56,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::QString, QMetaType::Int,   11,   60,
+    QMetaType::Void, QMetaType::Bool,    2,
+
+       0        // eod
+};
+
+void NavCoinGUI::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        NavCoinGUI *_t = static_cast<NavCoinGUI *>(_o);
+        Q_UNUSED(_t)
+        switch (_id) {
+        case 0: _t->receivedURI((*reinterpret_cast< const QString(*)>(_a[1]))); break;
+        case 1: _t->setNumConnections((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 2: _t->setNumBlocks((*reinterpret_cast< int(*)>(_a[1])),(*reinterpret_cast< const QDateTime(*)>(_a[2])),(*reinterpret_cast< double(*)>(_a[3])),(*reinterpret_cast< bool(*)>(_a[4]))); break;
+        case 3: _t->message((*reinterpret_cast< const QString(*)>(_a[1])),(*reinterpret_cast< const QString(*)>(_a[2])),(*reinterpret_cast< uint(*)>(_a[3])),(*reinterpret_cast< bool*(*)>(_a[4]))); break;
+        case 4: _t->message((*reinterpret_cast< const QString(*)>(_a[1])),(*reinterpret_cast< const QString(*)>(_a[2])),(*reinterpret_cast< uint(*)>(_a[3]))); break;
+        case 5: _t->replyFinished((*reinterpret_cast< QNetworkReply*(*)>(_a[1]))); break;
+        case 6: _t->replyVotingFinished((*reinterpret_cast< QNetworkReply*(*)>(_a[1]))); break;
+        case 7: _t->startVotingCounter(); break;
+        case 8: _t->setEncryptionStatus((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 9: { bool _r = _t->handlePaymentRequest((*reinterpret_cast< const SendCoinsRecipient(*)>(_a[1])));
+            if (_a[0]) *reinterpret_cast< bool*>(_a[0]) = std::move(_r); }  break;
+        case 10: _t->incomingTransaction((*reinterpret_cast< const QString(*)>(_a[1])),(*reinterpret_cast< int(*)>(_a[2])),(*reinterpret_cast< const CAmount(*)>(_a[3])),(*reinterpret_cast< const QString(*)>(_a[4])),(*reinterpret_cast< const QString(*)>(_a[5])),(*reinterpret_cast< const QString(*)>(_a[6]))); break;
+        case 11: _t->onCurrencySelection((*reinterpret_cast< QAction*(*)>(_a[1]))); break;
+        case 12: _t->gotoOverviewPage(); break;
+        case 13: _t->gotoHistoryPage(); break;
+        case 14: _t->gotoReceiveCoinsPage(); break;
+        case 15: _t->gotoSendCoinsPage((*reinterpret_cast< QString(*)>(_a[1]))); break;
+        case 16: _t->gotoSendCoinsPage(); break;
+        case 17: _t->gotoRequestPaymentPage(); break;
+        case 18: _t->gotoSignMessageTab((*reinterpret_cast< QString(*)>(_a[1]))); break;
+        case 19: _t->gotoSignMessageTab(); break;
+        case 20: _t->gotoVerifyMessageTab((*reinterpret_cast< QString(*)>(_a[1]))); break;
+        case 21: _t->gotoVerifyMessageTab(); break;
+        case 22: _t->openClicked(); break;
+        case 23: _t->updateStakingStatus(); break;
+        case 24: _t->updatePrice(); break;
+        case 25: _t->getVotingInfo(); break;
+        case 26: _t->repairWallet(); break;
+        case 27: _t->optionsClicked(); break;
+        case 28: _t->aboutClicked(); break;
+        case 29: _t->showDebugWindow(); break;
+        case 30: _t->showDebugWindowActivateConsole(); break;
+        case 31: _t->showHelpMessageClicked(); break;
+        case 32: _t->toggleStaking(); break;
+        case 33: _t->showNormalIfMinimized((*reinterpret_cast< bool(*)>(_a[1]))); break;
+        case 34: _t->showNormalIfMinimized(); break;
+        case 35: _t->toggleHidden(); break;
+        case 36: _t->detectShutdown(); break;
+        case 37: _t->showProgress((*reinterpret_cast< const QString(*)>(_a[1])),(*reinterpret_cast< int(*)>(_a[2]))); break;
+        case 38: _t->setTrayIconVisible((*reinterpret_cast< bool(*)>(_a[1]))); break;
+        default: ;
+        }
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        switch (_id) {
+        default: *reinterpret_cast<int*>(_a[0]) = -1; break;
+        case 5:
+            switch (*reinterpret_cast<int*>(_a[1])) {
+            default: *reinterpret_cast<int*>(_a[0]) = -1; break;
+            case 0:
+                *reinterpret_cast<int*>(_a[0]) = qRegisterMetaType< QNetworkReply* >(); break;
+            }
+            break;
+        case 6:
+            switch (*reinterpret_cast<int*>(_a[1])) {
+            default: *reinterpret_cast<int*>(_a[0]) = -1; break;
+            case 0:
+                *reinterpret_cast<int*>(_a[0]) = qRegisterMetaType< QNetworkReply* >(); break;
+            }
+            break;
+        case 11:
+            switch (*reinterpret_cast<int*>(_a[1])) {
+            default: *reinterpret_cast<int*>(_a[0]) = -1; break;
+            case 0:
+                *reinterpret_cast<int*>(_a[0]) = qRegisterMetaType< QAction* >(); break;
+            }
+            break;
+        }
+    } else if (_c == QMetaObject::IndexOfMethod) {
+        int *result = reinterpret_cast<int *>(_a[0]);
+        void **func = reinterpret_cast<void **>(_a[1]);
+        {
+            typedef void (NavCoinGUI::*_t)(const QString & );
+            if (*reinterpret_cast<_t *>(func) == static_cast<_t>(&NavCoinGUI::receivedURI)) {
+                *result = 0;
+                return;
+            }
+        }
+    }
+}
+
+const QMetaObject NavCoinGUI::staticMetaObject = {
+    { &QMainWindow::staticMetaObject, qt_meta_stringdata_NavCoinGUI.data,
+      qt_meta_data_NavCoinGUI,  qt_static_metacall, nullptr, nullptr}
+};
+
+
+const QMetaObject *NavCoinGUI::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *NavCoinGUI::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_NavCoinGUI.stringdata0))
+        return static_cast<void*>(this);
+    return QMainWindow::qt_metacast(_clname);
+}
+
+int NavCoinGUI::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QMainWindow::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 39)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 39;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 39)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 39;
+    }
+    return _id;
+}
+
+// SIGNAL 0
+void NavCoinGUI::receivedURI(const QString & _t1)
+{
+    void *_a[] = { nullptr, const_cast<void*>(reinterpret_cast<const void*>(&_t1)) };
+    QMetaObject::activate(this, &staticMetaObject, 0, _a);
+}
+struct qt_meta_stringdata_UnitDisplayStatusBarControl_t {
+    QByteArrayData data[7];
+    char stringdata0[88];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    qptrdiff(offsetof(qt_meta_stringdata_UnitDisplayStatusBarControl_t, stringdata0) + ofs \
+        - idx * sizeof(QByteArrayData)) \
+    )
+static const qt_meta_stringdata_UnitDisplayStatusBarControl_t qt_meta_stringdata_UnitDisplayStatusBarControl = {
+    {
+QT_MOC_LITERAL(0, 0, 27), // "UnitDisplayStatusBarControl"
+QT_MOC_LITERAL(1, 28, 17), // "updateDisplayUnit"
+QT_MOC_LITERAL(2, 46, 0), // ""
+QT_MOC_LITERAL(3, 47, 8), // "newUnits"
+QT_MOC_LITERAL(4, 56, 15), // "onMenuSelection"
+QT_MOC_LITERAL(5, 72, 8), // "QAction*"
+QT_MOC_LITERAL(6, 81, 6) // "action"
+
+    },
+    "UnitDisplayStatusBarControl\0"
+    "updateDisplayUnit\0\0newUnits\0onMenuSelection\0"
+    "QAction*\0action"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_UnitDisplayStatusBarControl[] = {
+
+ // content:
+       7,       // revision
+       0,       // classname
+       0,    0, // classinfo
+       2,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: name, argc, parameters, tag, flags
+       1,    1,   24,    2, 0x08 /* Private */,
+       4,    1,   27,    2, 0x08 /* Private */,
+
+ // slots: parameters
+    QMetaType::Void, QMetaType::Int,    3,
+    QMetaType::Void, 0x80000000 | 5,    6,
+
+       0        // eod
+};
+
+void UnitDisplayStatusBarControl::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        UnitDisplayStatusBarControl *_t = static_cast<UnitDisplayStatusBarControl *>(_o);
+        Q_UNUSED(_t)
+        switch (_id) {
+        case 0: _t->updateDisplayUnit((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 1: _t->onMenuSelection((*reinterpret_cast< QAction*(*)>(_a[1]))); break;
+        default: ;
+        }
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        switch (_id) {
+        default: *reinterpret_cast<int*>(_a[0]) = -1; break;
+        case 1:
+            switch (*reinterpret_cast<int*>(_a[1])) {
+            default: *reinterpret_cast<int*>(_a[0]) = -1; break;
+            case 0:
+                *reinterpret_cast<int*>(_a[0]) = qRegisterMetaType< QAction* >(); break;
+            }
+            break;
+        }
+    }
+}
+
+const QMetaObject UnitDisplayStatusBarControl::staticMetaObject = {
+    { &QLabel::staticMetaObject, qt_meta_stringdata_UnitDisplayStatusBarControl.data,
+      qt_meta_data_UnitDisplayStatusBarControl,  qt_static_metacall, nullptr, nullptr}
+};
+
+
+const QMetaObject *UnitDisplayStatusBarControl::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *UnitDisplayStatusBarControl::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_UnitDisplayStatusBarControl.stringdata0))
+        return static_cast<void*>(this);
+    return QLabel::qt_metacast(_clname);
+}
+
+int UnitDisplayStatusBarControl::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QLabel::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 2)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 2;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 2)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 2;
+    }
+    return _id;
+}
+QT_WARNING_POP
+QT_END_MOC_NAMESPACE
